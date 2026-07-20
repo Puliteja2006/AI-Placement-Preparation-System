f6ef6ab3563a71aac15773a6ee224ae23b2d0ae7
Puli Sai Srinivasa Teja <pulisaisrinivasateja@gmail.com>
2026-07-20 16:18:28 +0530

    Update AuthController.java

--- backend/src/main/java/com/aiplacement/controller/AuthController.java
+++ backend/src/main/java/com/aiplacement/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin(origins = "https://ai-placement-frontend-v6qc.onrender.com")
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin
